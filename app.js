7371fa6fc7e25b0dffe269bc66033d56bcc6891d
André Macedo <andremacedo.pv@gmail.com>
2022-04-16 11:36:05 -0300

    :sparkles: First request

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const db = require('./src/services/database')
 
+const router = require('./src/routes/routes')
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(router);
 
 db.sync()
 .then(result => {
     app.listen(3000);
 })
-
