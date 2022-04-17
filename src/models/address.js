0b540b78c29521bfd0d9a5d358bddb15eb89a0ee
Felipe <felipevaz444@gmail.com>
2022-04-17 17:25:09 -0300

    :sparkles: Model address

---
+++ src/models/address.js
@@ -0,0 +1,35 @@
+const sequelize = require('sequelize')
+const db = require('../services/database')
+
+const Address = db.define('address', {
+  id: {
+    type: sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  country: {
+    type: sequelize.STRING,
+    allowNull: false,
+  },
+  state: {
+    type: sequelize.STRING,
+    allowNull: false,
+  },
+  city: {
+    type: sequelize.STRING,
+    allowNull: false,
+  },
+  street: {
+    type: sequelize.STRING,
+    allowNull: false,
+  },
+  neighborhood: {
+    type: sequelize.STRING,
+    allowNull: false,
+  },
+  number: {
+    type: sequelize.STRING,
+    allowNull: false,
+  }
+})
