f2ea821252a1d8a18ebf39d57bf2882925bc1e83
André Macedo <andremacedo.pv@gmail.com>
2022-05-09 11:49:41 -0300

    :bricks: Set response headers

--- app.js
+++ app.js
@@ -11,9 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(router);
 
 db.sync()
 .then(result => {
-    app.listen(3000);
+    app.listen(3333);
 })
