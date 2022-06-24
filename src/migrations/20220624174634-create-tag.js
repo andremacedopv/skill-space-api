4d3820762559ead611deccdb67b429017b0bc7e3
Felipe <felipevaz444@gmail.com>
2022-06-24 14:55:20 -0300

    :card_file_box: Model tag

---
+++ src/migrations/20220624174634-create-tag.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('tags', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+          type: Sequelize.STRING,
+          allowNull: false
+      },
+      updatedAt: Sequelize.DATE,
+      createdAt: Sequelize.DATE
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('tags');
+  }
+};
---
+++ src/models/tag.js
@@ -0,0 +1,17 @@
+const sequelize = require('sequelize')
+const db = require('../services/database')
+
+const Tag = db.define('tags', {
+  id: {
+    type: sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: sequelize.STRING,
+    allowNull: false,
+  }
+})
+
+module.exports = Tag
