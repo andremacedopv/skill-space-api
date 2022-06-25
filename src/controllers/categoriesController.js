266d934ff387bc608f0de7260179d70e0c8ebeed
André Macedo <andremacedo.pv@gmail.com>
2022-06-25 18:23:59 -0300

    :sparkles: Category and ActivityType CRUD

---
+++ src/controllers/categoriesController.js
@@ -0,0 +1,72 @@
+// Imports
+const Category = require('../models/category');
+const Activity = require('../models/activity')
+
+// Methods
+exports.index = (req, res, next) => {
+    Category.findAll()
+    .then(category => {
+        res.json({ categories: category });
+    })
+    .catch(e => {
+        console.log(e)
+        res.status(500).json({ error: e })
+    })  
+}
+
+exports.create = (req, res, next) => {
+    const category = req.body;
+    Category.create({
+        name: category.name,
+        description: category.description,
+    })
+    .then(newCategory => {
+        res.json({ category: newCategory.dataValues });
+    })
+    .catch(e => {
+        console.log(e)
+        res.status(422).json({ error: e })
+    }) 
+}
+
+exports.update = (req, res, next) => {
+    const newCategory = req.body;
+    Category.findByPk(req.params.id)
+    .then(category => {
+        category.name = newCategory.name? newCategory.name : category.name;
+        category.description = newCategory.description? newCategory.description : category.description;
+        return category.save()
+    })
+    .then(response => {
+        res.json({ category: response.dataValues });
+    })
+    .catch(e => {
+        console.log(e)
+        res.status(422).json({ error: e })
+    }) 
+}
+
+exports.show = (req, res, next) => {
+    Category.findByPk(req.params.id, { include: Activity })
+    .then(category => {
+        res.json({ category: category })
+    })
+    .catch(e => {
+        console.log(e)
+        res.status(422).json({ error: e })
+    }) 
+}
+
+exports.delete = (req, res, next) => {
+    Category.findByPk(req.params.id)
+    .then(category => {
+        return category.destroy()
+    })
+    .then(response => {
+        res.json({ message: 'Tipo de Atividade deletado com sucesso' })
+    })
+    .catch(e => {
+        console.log(e)
+        res.status(422).json({ error: e })
+    }) 
+}
--- src/seeders/20220625205228-category.js
+++ src/seeders/20220625205228-category.js
@@ -4,7 +4,7 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     let d = new Date(); 
     let date = d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
-   await queryInterface.bulkInsert('activityTypes', [
+   await queryInterface.bulkInsert('categories', [
      {
        name: "Topic 1",
        description: "A nice topic.",
