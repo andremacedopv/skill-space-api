6f7f62da636064e2526fbeac1ae024da27644559
Felipe <felipevaz444@gmail.com>
2022-07-16 18:37:06 -0300

    :bug: Fix Typo

--- src/migrations/20220705234548-activitySubmission.js
+++ src/migrations/20220705234548-activitySubmission.js
@@ -24,6 +24,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('activityUsers');
+    await queryInterface.dropTable('activitySubmissions');
   }
 };
