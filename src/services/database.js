503c3b369de07e6b1353d7d8c218de812871964a
André Macedo <andremacedo.pv@gmail.com>
2022-09-29 18:21:39 -0300

    :rocket: Database configuration

--- src/services/database.js
+++ src/services/database.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('skill-space', process.env.DB_USER, process.env.DB_PASSWORD, {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+// const sequelize = new Sequelize('skill-space', process.env.DB_USER, process.env.DB_PASSWORD, {
+//     dialect: 'mysql',
+//     host: 'localhost'
+// });
+
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 
 module.exports = sequelize;
